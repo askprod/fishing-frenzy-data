d711317f588c799182e550ca5cbe0a9436b504c0
sirgm <paul.trin-dinh@simplebo.fr>
2025-03-28 13:43:59 +0100

    fix(scheduler): used system to run rails commands

--- app/workers/worker.rb
+++ app/workers/worker.rb
@@ -3,7 +3,7 @@ require "rufus-scheduler"
 scheduler = Rufus::Scheduler.new
 
 scheduler.every "1m" do
-  StatusCheckerJob.perform_later
+  system("rails runner StatusCheckerJob.perform_later")
 end
 
 scheduler.join
