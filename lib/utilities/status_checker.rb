14fd2217c123b35f1e25bb602d46540eaaa53207
sirgm <paul.trin-dinh@simplebo.fr>
2025-04-02 14:12:34 +0200

    feat(cache): updated favicon and configured solid cache, now used in status checker

--- lib/utilities/status_checker.rb
+++ lib/utilities/status_checker.rb
@@ -1,77 +1,63 @@
 class Utilities::StatusChecker
-  # TODO: Refactor class to store status in cache (solid cache?) or DB instead of file
-
   URL = "https://fishingfrenzy.co".freeze
   CHANNEL_KEY = "status_checker_channel"
-  STATUS_FILE_PATH = Rails.root.join("lib", "utilities", "server_status.yml")
+  CACHE_KEY = "server_status"
 
   def self.check_status(forced_status: nil)
-    Rails.logger.info "Checking status for #{URL}...".red
-
+    Rails.logger.info "Checking status for #{URL}..."
     response_code = forced_status ? forced_status : RestClient.get(URL).code
-    status = self.code_to_status(response_code)
-    write_status_to_file(status)
-    channel_status(status)
-    log_status(status)
+    status = code_to_status(response_code)
+    update_status(status)
 
   rescue RestClient::ServiceUnavailable
-    write_status_to_file(:maintenance)
-    channel_status(:maintenance)
-    log_status(:maintenance)
+    update_status(:maintenance)
   rescue RestClient::InternalServerError
-    write_status_to_file(:error)
-    channel_status(:error)
-    log_status(:error)
+    update_status(:error)
+  end
+
+  def self.update_status(status)
+    write_status_to_cache(status)
+    send_status_to_channel(status)
+    log_status(status)
   end
 
   def self.up?
-    read_status_from_file == :up
+    read_status_from_cache == :up
   end
 
   def self.maintenance?
-    read_status_from_file == :maintenance
+    read_status_from_cache == :maintenance
   end
 
   def self.error?
-    read_status_from_file == :error
+    read_status_from_cache == :error
   end
 
   private
 
   def self.log_status(status)
-    Rails.logger.info "Updated status for #{URL} - #{status.to_s.upcase}.".red
+    Rails.logger.info "Updated status for #{URL} - #{status.to_s.upcase}."
   end
 
-  def self.channel_status(status)
+  def self.send_status_to_channel(status)
     ActionCable.server.broadcast(CHANNEL_KEY, { status: status })
   end
 
-  def self.write_status_to_file(status)
-    data = { status: status.to_s }
-    File.open(STATUS_FILE_PATH, "w") do |file|
-      file.write(data.to_yaml)
-    end
+  def self.write_status_to_cache(status)
+    Rails.cache.write(CACHE_KEY, status.to_s, expires_in: 5.minutes) # Adjust expiration as needed
   end
 
-  def self.read_status_from_file
-    if File.exist?(STATUS_FILE_PATH)
-      data = YAML.load_file(STATUS_FILE_PATH)
-      data[:status].to_sym if data && data[:status]
-    else
-      :unknown
-    end
+  def self.read_status_from_cache
+    cached_status = Rails.cache.read(CACHE_KEY)
+    cached_status ? cached_status.to_sym : :unknown
   end
 
   def self.code_to_status(code)
     case code
-    when 200
-      :up
-    when 500
-      :error
-    when 503
-      :maintenance
-    else
-      :unknown
+    when 200 then :up
+    when 500 then :error
+    when 503 then :maintenance
+    else :unknown
     end
   end
 end
